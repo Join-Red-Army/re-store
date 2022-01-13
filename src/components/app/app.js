d4d1bdeac1888c84ccde9c0a458419d5080e4904
Join-Red-Army <this.is.my.address.for.mail@ya.ru>
2022-01-14 00:30:42 +0300

    Стили + компоненты shop-header, shoppint-cart-table

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Routes, Route, Switch } from 'react-router-dom';
 import { HomePage, CartPage } from '../pages';
+import ShopHeader from '../shop-header.js/shop-header';
 import './app.css';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path='/' element={<HomePage />} />
-      <Route path='/cart' element={<CartPage />} />
-    </Routes>
+    <main role="main" className="container">
+      <ShopHeader numItems={5} total={129}/>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/cart' element={<CartPage />} />
+      </Routes>
+    </main>
   )
 };
 
--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -1,8 +1,18 @@
 // отвечает за получение данных о книгах
 
 const books = [
-  {id: 1, title: 'Alice in Wonderland', author: 'Lewis Carroll'},
-  {id: 2, title: 'Call of Cthulhu', author: 'Howard Lovecraft'},
+  {
+    id: 1, title: 'Alice in Wonderland',
+    author: 'Lewis Carroll',
+    price: 32, 
+    coverImage: 'https://images-na.ssl-images-amazon.com/images/I/419Uq8a-y+L._SX298_BO1,204,203,200_.jpg'
+  },
+  {
+    id: 2, 
+    title: 'Call of Cthulhu',
+    author: 'Howard Lovecraft',
+    price: 29, coverImage: 'https://images-na.ssl-images-amazon.com/images/I/51RpsrlmAeL._SX311_BO1,204,203,200_.jpg'
+  },
 ];
 
 export default class BookstoreService {

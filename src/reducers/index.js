cbf3e1850067cca2a9c4576cdd772b6a10723ad0
Join-Red-Army <this.is.my.address.for.mail@ya.ru>
2022-01-16 01:52:58 +0300

    Подключение нового компонента к redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,20 +1,36 @@
 const initialState = {
   books: [],
   loading: true,
-  error: null
+  error: null,
+  cartItems: [
+    {
+      id: 1,
+      name: 'Book 1',
+      count: 3,
+      total: 150  
+    },
+
+    {
+      id: 2,
+      name: 'Book 2',
+      count: 2,
+      total: 70  
+    },
+  ],
+  orderTotal: 220
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
 
     case 'FETCH_BOOKS_REQUEST':
-      return { books: [], loading: true, error: null };
+      return { ...state, books: [], loading: true, error: null };
 
     case 'FETCH_BOOKS_SUCCESS': 
-      return { books: action.payload, loading: false, error: null };
+      return { ...state, books: action.payload, loading: false, error: null };
 
     case 'FETCH_BOOKS_FAILURE':
-      return { books: [], loading: false, error: action.payload };
+      return { ...state, books: [], loading: false, error: action.payload };
 
     default: return state;
   }
